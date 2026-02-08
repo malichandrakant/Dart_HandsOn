06d88882dbef6e2e216c0d9817c62e3fa196d3b4
Mali Chandrakant <161549812+malichandrakant@users.noreply.github.com>
2026-02-08 15:14:41 +0530

    add ordered collection of elements using List

---
+++ list.dart
@@ -0,0 +1,13 @@
+void main() {
+  List fruits = ["Apple", "Banana", "Mango", "Grapes"];
+
+  print(fruits);
+
+  List numbers = [1, 2, 3, 4, 5];
+
+  print(numbers);
+
+  List mixedList = ["Hello", 42, true, 3.14];
+
+  print(mixedList);
+}
