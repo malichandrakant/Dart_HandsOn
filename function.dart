a680b0ee0e9f66bdc880d193b971f2c86a9ddd44
Mali Chandrakant <161549812+malichandrakant@users.noreply.github.com>
2026-02-08 15:25:57 +0530

    add function uses in dart

---
+++ function.dart
@@ -0,0 +1,15 @@
+// Function with return value
+
+int add(int a, int b) {
+  return a + b;
+}
+
+void greet(String name) {
+  print("Hello, $name!");
+}
+
+void main() {
+  int result = add(3, 5);
+  print("The sum is: $result");
+  greet("Chandu");
+}
