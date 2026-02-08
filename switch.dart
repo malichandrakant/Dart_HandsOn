bd007a17a55cc8080624d6c461ae7f40e4e2c40e
Mali Chandrakant <161549812+malichandrakant@users.noreply.github.com>
2026-02-08 15:59:49 +0530

    add switch statement in dart

---
+++ switch.dart
@@ -0,0 +1,17 @@
+void main() {
+  int day = 2;
+
+  switch (day) {
+    case 1:
+      print("Monday");
+      break;
+    case 2:
+      print("Tuesday");
+      break;
+    case 3:
+      print("Wednesday");
+      break;
+    default:
+      print("Another day");
+  }
+}
